af5dfa7ca3872ce6478a64d868c22f9ec6d4a0a8
caidevoficial <caidev.sama@gmail.com>
2020-02-27 00:34:03 -0300

    mejora de codigo

--- Bhaskara/bhaskara1.js
+++ Bhaskara/bhaskara1.js
@@ -4,7 +4,6 @@ By CaiDev.
 */
 function bhaskara()
 {
-//import Math();
 
 var terminoA = parseInt(document.getElementById('terminoA').value);
 var terminoB = parseInt(document.getElementById('terminoB').value);
@@ -20,7 +19,7 @@ var solucion_x2;
 
 if (terminoA!=0){
 
-	determinante = parseFloat(Math.sqrt(((terminoB*terminoB) - (4*terminoA*terminoC)));
+	determinante = parseFloat(Math.sqrt(((terminoB*terminoB) - (4*terminoA*terminoC))));
 	if (determinante >=0){
 		formulaCuadratica_x1 = ((-(terminoB))+determinante)/(2*terminoA);
 		formulaCuadratica_x2 =  ((-(terminoB))-determinante)/(2*terminoA);	
@@ -33,7 +32,7 @@ if (terminoA!=0){
 	solucion_x1 = formulaCuadratica_x1;
 	solucion_x2 = formulaCuadratica_x2;
 
-	function decimalToFraction(value, donly = true) {
+	/*function decimalToFraction(value, donly = true) {
   var tolerance = 1.0E-6; // a partir de cuantas decimales se hace el redondeo
   var h1 = 1;
   var h2 = 0;
@@ -42,7 +41,7 @@ if (terminoA!=0){
   var negative = false;
   var i;
 
-  if (parseInt(value) == value) { // si el valor es un número entero, detener el código
+  if (parseInt(value) == value) { // si el valor es un número entero, detiene el código
     return value;
   } else if (value < 0) {
     negative = true;
@@ -69,9 +68,11 @@ if (terminoA!=0){
   } while (Math.abs(value - h1 / k1) > value * tolerance);
 
   return (negative ? "-" : '') + ((donly & (i != 0)) ? i + ' ' : '') + (h1 == 0 ? '' : h1 + "/" + k1);
-}
-	document.getElementById('x1').value = decimalToFraction(solucion_x1);
-	document.getElementById('x2').value = decimalToFraction(solucion_x2);
+}*/
+	//document.getElementById('x1').value = decimalToFraction(solucion_x1);
+	//document.getElementById('x2').value = decimalToFraction(solucion_x2);
+	document.getElementById('x1').value = solucion_x1;
+	document.getElementById('x2').value = solucion_x2;
 
 
 }//FIN DE LA FUNCIÓN Bhaskara
