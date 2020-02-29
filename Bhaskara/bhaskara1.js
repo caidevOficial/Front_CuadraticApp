635fdd6d1a241ea7822b3c2b9fcfd4180dcc31fa
caidevoficial <caidev.sama@gmail.com>
2020-02-29 19:58:41 -0300

    mejora de mensaje de solucion

--- Bhaskara/bhaskara1.js
+++ Bhaskara/bhaskara1.js
@@ -1,7 +1,12 @@
 /*
-Resolver una ecuacion de segundo grado mediante bhaskara.
+Resolver una ecuacion de segundo grado mediante resolvente de bhaskara.
+Funciones:
+  1 - Si el coeficiente del termino cuadratico es 0, denotara un mensaje de error advirtiendo que la division principal sera por 0, por lo tanto no se puede efectuar.
+  2 - Si la determinante da un numero negativo ( antes de efectuar su raiz cuadrada) dara un mensaje de error advirtiendo que las raices no son reales, sino imaginarias.
+  3 - en caso de no cumplirse los puntos antes mencionados, la ecuacion se resolvera sin problemas, denotando sus dos soluciones!
 By CaiDev.
 */
+
 function bhaskara()
 {
 
@@ -25,11 +30,15 @@ if (terminoA!=0){
 		formulaCuadratica_x2 =  ((-(terminoB))-determinante)/(2*terminoA);	
 	}
 	else{
-		alert("No tiene soluciónes reales - raices imaginarias");
+    formulaCuadratica_x1 = "Raíz imaginaria";
+    formulaCuadratica_x2 = "Raíz imaginaria";
+    alert("No tiene soluciónes reales - raices imaginarias");
 	}
 	
 }
 else {
+  formulaCuadratica_x1 = "Ecuación Inválida";
+  formulaCuadratica_x2 = "Ecuación Inválida";
 	alert("Si el termino A es 0, pretenderas dividir tambien por 0 y eso NO SE PUEDE!");
 }
 	solucion_x1 = formulaCuadratica_x1;
@@ -79,9 +88,3 @@ else {
 
 
 }//FIN DE LA FUNCIÓN Bhaskara
-
-/*function pooShenLoo(){
-
-
-}
-*/
