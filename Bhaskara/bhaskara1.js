2242f7dc60c96198b865d66a6f8ed069b38ae2b3
caidevoficial <caidev.sama@gmail.com>
2020-04-10 23:11:43 -0300

    Limpieza de codigo obsoleto

--- Bhaskara/bhaskara1.js
+++ Bhaskara/bhaskara1.js
@@ -30,61 +30,23 @@ if (terminoA!=0){
 		formulaCuadratica_x2 =  ((-(terminoB))-determinante)/(2*terminoA);	
 	}
 	else{
-    formulaCuadratica_x1 = "Raíz imaginaria";
-    formulaCuadratica_x2 = "Raíz imaginaria";
-    alert("No tiene soluciónes reales - raices imaginarias");
+      formulaCuadratica_x1 = "Raíz imaginaria";
+      formulaCuadratica_x2 = "Raíz imaginaria";
+      alert("No tiene soluciónes reales - raices imaginarias");
 	}
 	
 }
 else {
-  formulaCuadratica_x1 = "Ecuación Inválida";
-  formulaCuadratica_x2 = "Ecuación Inválida";
-	alert("Si el termino A es 0, pretenderas dividir tambien por 0 y eso NO SE PUEDE!");
+      formulaCuadratica_x1 = "Ecuación Inválida";
+      formulaCuadratica_x2 = "Ecuación Inválida";
+    	alert("Si el termino A es 0, pretenderas dividir tambien por 0 y eso NO SE PUEDE!");
 }
-	solucion_x1 = formulaCuadratica_x1;
-	solucion_x2 = formulaCuadratica_x2;
+solucion_x1 = formulaCuadratica_x1;
+solucion_x2 = formulaCuadratica_x2;
 
-	/*function decimalToFraction(value, donly = true) {
-  var tolerance = 1.0E-6; // a partir de cuantas decimales se hace el redondeo
-  var h1 = 1;
-  var h2 = 0;
-  var k1 = 0;
-  var k2 = 1;
-  var negative = false;
-  var i;
-
-  if (parseInt(value) == value) { // si el valor es un número entero, detiene el código
-    return value;
-  } else if (value < 0) {
-    negative = true;
-    value = -value;
-  }
-
-  if (donly) {
-    i = parseInt(value);
-    value -= i;
-  }
-
-  var b = value;
-
-  do {
-    var a = Math.floor(b);
-    console.log(a)
-    var aux = h1;
-    h1 = a * h1 + h2;
-    h2 = aux;
-    aux = k1;
-    k1 = a * k1 + k2;
-    k2 = aux;
-    b = 1 / (b - a);
-  } while (Math.abs(value - h1 / k1) > value * tolerance);
-
-  return (negative ? "-" : '') + ((donly & (i != 0)) ? i + ' ' : '') + (h1 == 0 ? '' : h1 + "/" + k1);
-}*/
-	//document.getElementById('x1').value = decimalToFraction(solucion_x1);
-	//document.getElementById('x2').value = decimalToFraction(solucion_x2);
-	document.getElementById('x1').value = solucion_x1;
-	document.getElementById('x2').value = solucion_x2;
+	
+document.getElementById('x1').value = solucion_x1;
+document.getElementById('x2').value = solucion_x2;
 
 
 }//FIN DE LA FUNCIÓN Bhaskara
